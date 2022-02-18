417eb9c58715171217077bd3ae5ba9f2e52f45c6
Jedguy/Kenan Doreau <71514311+Jedguy@users.noreply.github.com>
2022-02-18 19:20:57 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,86 +1,42 @@
+import 'dart:io';                            // Add this import.
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const MaterialApp(
+      home: WebViewApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-  //test ceci est un test
-  // This widget is the root of your application.
+class WebViewApp extends StatefulWidget {
+  const WebViewApp({Key? key}) : super(key: key);
+
+  @override
+  State<WebViewApp> createState() => _WebViewAppState();
+}
+
+class _WebViewAppState extends State<WebViewApp> {
+  // Add from here ...
+  @override
+  void initState() {
+    if (Platform.isAndroid) {
+      WebView.platform = SurfaceAndroidWebView();
+    }
+    super.initState();
+  }
+  // ... to here.
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-     home: BaseApp(),
-      routes: <String, WidgetBuilder>{
-        '/viewListMaterial' : (context) => viewListMaterial(),
-        '/viewListAgent' : (context) => viewListAgent(),
-        '/viewAgent' : (context) => viewAgent(),
-      },
-    );
-  }
-}
-
-class BaseApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context){
-    // ignore: dead_code
     return Scaffold(
-      appBar:  AppBar(title: Text("MSPR Application"),),
-      body: Center(
-        child:Column(
-          children: <Widget>[
-            FlatButton(
-              child: Text("Liste du matériel"),
-              color: Colors.lightBlueAccent,
-              onPressed: () {
-                Navigator.pushNamed(context, '/viewListMaterial');
-              },
-            ),
-            FlatButton(
-               child: Text("Liste des agents "),
-               color: Colors.lightBlueAccent,
-               onPressed: () {
-                 Navigator.pushNamed(context, '/viewListAgent');
-               },
-        ),
-            FlatButton(
-              child: Text("Descriptif de l'agent"),
-              color: Colors.lightBlueAccent,
-              onPressed: () {
-                Navigator.pushNamed(context, '/viewAgent');
-              },
-            ),
-          ],
-        ),
-      )
-    );
-  }
-}
-
-class viewListMaterial extends StatelessWidget {
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(title: Text("Liste du matériel"),),
-    );
-  }
-}
-
-class viewListAgent extends StatelessWidget {
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(title: Text("Liste des agents"),),
-    );
-  }
-}
-
-class viewAgent extends StatelessWidget {
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(title: Text("Descriptif de l'agent"),),
+      appBar: AppBar(
+        title: const Text('Flutter WebView'),
+      ),
+      body: const WebView(
+        initialUrl: 'https://louen-peschard.github.io/louen-peschard/home.html',
+      ),
     );
   }
 }
